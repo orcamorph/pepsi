7613919b0152d3ee1ac0a50768defd346fa0e729
Onur Sahin <orcamorf@gmail.com>
2024-02-13 12:56:22 +0300

    a css project about pepsi website

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function changeImg(newSrc) {
+    document.querySelector(".pepsi").src = newSrc;
+}
+
+function changeBackground(newColor) {
+    var section = document.querySelector(".section");
+    section.style.background = newColor;
+}
